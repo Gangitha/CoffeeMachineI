8083f7eccc33d1f74fdbd9b06d471685002815c6
Gangitha Diwanjana <91070180+Gangitha@users.noreply.github.com>
2021-11-19 16:18:35 +0530

    Coffee machine code

---
+++ class CoffeeMachineInterface.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+class CoffeeMachineInterface {
+public:
+    void chooseFirstSelection() {
+        cout << "Coffee flavor 1(new machine)" << "\n";
+    }
+    void chooseSecondSelection() {
+        cout << "Coffee flavor 2(new machine)" << "\n";
+    }
+};
+
+
